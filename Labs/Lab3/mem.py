3a0696db8d23f9fe8c3d48602f7a994631adaa38
KirillYuk <qwertyunisky@gmail.com>
2026-03-28 18:14:59 +0200

    implement lru strategy

--- Labs/Lab3/mem.py
+++ Labs/Lab3/mem.py
@@ -1,25 +1,37 @@
+import time
 from functools import wraps
 
-def memo(max_size=None):
+
+def memo(max_size=None, strategy="lru"):
     cache = {}
+    access_time = {}
 
     def decorator(func):
         @wraps(func)
-
         def wrapper(*args, **kwargs):
             key = str(args) + str(kwargs)
+            now = time.time()
 
             if key in cache:
+                access_time[key] = now
                 return cache[key]
-            
+
             res = func(*args, **kwargs)
 
             if max_size is not None and len(cache) >= max_size:
-                first_key = next(iter(cache))
-                del cache[first_key]
-                
+                if strategy == "lru":
+                    to_del = min(access_time, key=access_time.get)
+                else:
+                    to_del = next(iter(cache))
+
+                del cache[to_del]
+                del access_time[to_del]
+
             cache[key] = res
+            access_time[key] = now
 
             return res
+
         return wrapper
+
     return decorator
--- Labs/Lab3/test.py
+++ Labs/Lab3/test.py
@@ -1,14 +1,12 @@
 from mem import memo
 
-@memo(max_size=2)
-def multiply(x, y=1):
+@memo(max_size=2, strategy="lru")
+def multiply(x, y=2):
     print("calc", x, "*", y)
     return x * y
 
+print(multiply(1))
+print(multiply(2))
+print(multiply(1))
 print(multiply(3))
-print(multiply(4))
-print(multiply(5))
-print(multiply(3))
-print(multiply(5, 5))
-print(multiply(5, 3))
-print(multiply(5, 5))
+print(multiply(2))
