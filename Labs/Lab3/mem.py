bedfa78be2f1041aee8df3fdef8759549b9e18cf
KirillYuk <qwertyunisky@gmail.com>
2026-03-29 12:17:41 +0300

    implement lfu strategy

--- Labs/Lab3/mem.py
+++ Labs/Lab3/mem.py
@@ -5,6 +5,7 @@ from functools import wraps
 def memo(max_size=None, strategy="lru"):
     cache = {}
     access_time = {}
+    use_count = {}
 
     def decorator(func):
         @wraps(func)
@@ -14,6 +15,7 @@ def memo(max_size=None, strategy="lru"):
 
             if key in cache:
                 access_time[key] = now
+                use_count[key] = use_count[key] + 1
                 return cache[key]
 
             res = func(*args, **kwargs)
@@ -21,14 +23,18 @@ def memo(max_size=None, strategy="lru"):
             if max_size is not None and len(cache) >= max_size:
                 if strategy == "lru":
                     to_del = min(access_time, key=access_time.get)
+                elif strategy == "lfu":
+                    to_del = min(use_count, key=use_count.get)
                 else:
                     to_del = next(iter(cache))
 
                 del cache[to_del]
                 del access_time[to_del]
+                del use_count[to_del]
 
             cache[key] = res
             access_time[key] = now
+            use_count[key] = 1
 
             return res
 
--- Labs/Lab3/test.py
+++ Labs/Lab3/test.py
@@ -1,14 +1,13 @@
 from mem import memo
 
-@memo(max_size=2, strategy="lru")
+@memo(max_size=2, strategy="lfu")
 def multiply(x, y=2):
     print("calc", x, "*", y)
     return x * y
 
 print(multiply(1))
-print(multiply(2))
 print(multiply(1))
-print(multiply(3))
 print(multiply(2))
 print(multiply(3))
+print(multiply(2))
 print(multiply(1))
