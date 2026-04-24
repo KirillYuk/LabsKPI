dc32579343651c40eae6f1e6e3a7b1ba9159147d
KirillYuk <qwertyunisky@gmail.com>
2026-04-24 15:46:06 +0300

    callback error handling

--- Labs/Lab5/async_map.py
+++ Labs/Lab5/async_map.py
@@ -1,16 +1,19 @@
 import asyncio
 
 
-# def sync_map(arr, fn):
-#     result = []
-#     for item in arr:
-#         result.append(fn(item))
-#     return result
+def sync_map(arr, fn):
+    result = []
+    for item in arr:
+        result.append(fn(item))
+    return result
 
 
 def async_map_callback(arr, fn, callback):
     async def run():
-        results = await asyncio.gather(*[fn(item) for item in arr])
-        callback(results)
+        try:
+            results = await asyncio.gather(*[fn(item) for item in arr])
+            callback(None, results)
+        except Exception as e:
+            callback(e, None)
 
     asyncio.run(run())
--- Labs/Lab5/main.py
+++ Labs/Lab5/main.py
@@ -6,7 +6,10 @@ async def double(x):
     await asyncio.sleep(1) 
     return x * 2
 
-def on_done(result):
-    print(result)
+def on_done(error, result):
+    if error:
+        print("error: ", error)
+    else:
+        print("result: ", result)
 
-async_map_callback([1, 2, 3], double, on_done)
+async_map_callback([2, 3, 4], double, on_done)
