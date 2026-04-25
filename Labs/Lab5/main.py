046e8467a05fce1a159aeb987e4d8c5d5e4f07d1
KirillYuk <qwertyunisky@gmail.com>
2026-04-26 01:23:55 +0300

    demo + doc

--- Labs/Lab5/main.py
+++ Labs/Lab5/main.py
@@ -1,13 +1,44 @@
 import time
 import asyncio
-from async_map import async_map_promise, async_map_await
+from async_map import async_map_promise, async_map_await, sync_map, async_map_callback
 
 
 async def double(x):
     await asyncio.sleep(1) 
     return x * 2
 
-async def main():
+
+print("sync")
+print(sync_map([1,2,3], lambda x: x*2))
+
+
+print("callback")
+def on_done(err, result):
+    if err:
+        print("err: ", err)
+    else:
+        print("resulr: ", result)
+async_map_callback([1, 2, 3], double, on_done)
+
+
+print("promise")
+async def my_promise():
+    start = time.time()
+    result = await async_map_promise([1,2,3], double)
+    print("result", result, time.time() - start, "sec")
+asyncio.run(my_promise())
+
+
+print("async/await")
+async def my_await():
+    start = time.time()
+    result = await async_map_await([1,2,3], double)
+    print("result: ", result, time.time() - start, "sec")
+asyncio.run(my_await())
+
+
+print("canceling")
+async def my_cancel():
     cancel = asyncio.Event()
     
     async def cancel_after():
@@ -19,4 +50,4 @@ async def main():
     result = await async_map_await([1, 2], double, cancel)
     print("result", result)
     
-asyncio.run(main())
+asyncio.run(my_cancel())
