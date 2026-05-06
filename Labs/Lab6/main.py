cfaf587b72dc4b274df60d8d11e1ee1a5fb44eae
KirillYuk <qwertyunisky@gmail.com>
2026-05-06 18:04:02 +0300

    demo cases

--- Labs/Lab6/main.py
+++ Labs/Lab6/main.py
@@ -6,6 +6,27 @@ from stream import generate_data, read_stream, process_stream, async_process_str
 def to_upper(item):
     return item.upper()
 
+def add_index(item):
+    return "[processed]", item
+
+
+
+for item in read_stream(generate_data(5)):
+    print(item)
+    
+
+pipeline = process_stream(read_stream(generate_data(5)), to_upper)
+for item in pipeline:
+    print(item)
+    
+    
+async def demo_async():
+    source = async_read_stream(generate_data(5))
+    async for item in async_process_stream(source, add_index):
+        print(item)
+asyncio.run(demo_async()) 
+
+
 tracemalloc.start()
 data = list(generate_data(100000))
 current, peak = tracemalloc.get_traced_memory()
